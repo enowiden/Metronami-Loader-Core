19bb78f938be2f962cfd7c78ddd141ff727fe116
Dylan Kok <tenkotofu@gmail.com>
2021-09-21 20:39:46 +0800

    Added files from Metronami-Loader

---
+++ bootloader.js
@@ -0,0 +1,73 @@
+module.exports = async (appRoot) => {
+    const execa = require('execa')
+    const chalk = require('chalk')
+    const open = require('open')
+    const { getUpdate, applyUpdate } = require('./updater')
+
+    // Get current version number from Metronami package
+    let currentVersion
+    try {
+        currentVersion = require(appRoot + '/metronami/package.json').version
+    } catch (e) {
+        currentVersion = '0.0.0'
+    }
+
+    // Launch Browser flag
+    let launchBrowser = true;
+    let TimeToLaunch = 8000
+
+    // Installation Mode
+    // Will activate if currentVersion is 0.0.0
+    if (currentVersion === '0.0.0') {
+        console.log(chalk.whiteBright('[LOADER] New installation detected. Downloading additional files...'))
+
+        await getUpdate(appRoot).then(async (latestTag) => {
+            await applyUpdate(appRoot, latestTag)
+            console.log(chalk.green(`[LOADER] Metronami ${latestTag} has been installed.`))
+
+            // Set TimeToLaunch longer to accomodate installation time
+            TimeToLaunch = 20000
+        })
+    }
+
+        while (true) {
+            try {
+                // Start the browser if flag is set
+                if (launchBrowser === true) {
+                    launchBrowser = false
+                    setTimeout(() => {
+                        console.log(`${chalk.whiteBright('Launching Metronami in your browser... If the page does not load, give it awhile and refresh the page.')}`)
+                        open(`http://localhost:36554`)
+
+                        setTimeout(() => {
+                            console.log(`${chalk.cyan('Nothing happened? ')}${chalk.whiteBright(`Type http://localhost:36554 in your web browser's address bar to access Metronami.`)}`)
+                        }, 5000)
+                    }, TimeToLaunch)
+                }
+                const { stdout } = await execa('npm run app-start', { stdio: 'inherit' })
+            } catch (exitData) {
+                if (exitData.exitCode === 0) {
+                    process.exit()
+                }
+
+                // 200 = Restart signal
+                if (exitData.exitCode === 200) {
+                    console.log(chalk.white('[LOADER] Restart signal received. Restarting...'))
+                    continue
+                }
+
+                // 201 - Update mode
+                if (exitData.exitCode === 201) {
+                    console.log(chalk.white('[LOADER] Entering Update mode'))
+                    console.log(chalk.redBright('While Metronami is updating, do not interrupt or disturb this process.'))
+                    const getCurrentVersion = await getUpdate(appRoot)
+                    await applyUpdate(appRoot, getCurrentVersion)
+                    console.log(chalk.green('[LOADER] Update complete. Restarting...'))
+                    continue
+                }
+
+                // If everything else, throw the error
+                throw exitData
+            }
+        }
+}
---
+++ updater.js
@@ -0,0 +1,73 @@
+const semver = require('semver')
+const getTagFor = require('get-github-tag')
+const download = require('download')
+
+const extract = require('extract-zip')
+
+const chalk = require('chalk')
+
+const fs = require('fs')
+
+const ncp = require('ncp').ncp
+ncp.limit = 16
+
+const getUpdate = (appRoot) => {
+    let currentVersion
+    try {
+        currentVersion = require(appRoot + '/metronami/package.json').version || '0.0.0'
+    } catch (e) {
+        currentVersion = '0.0.0'
+    }
+    
+    return new Promise((resolve) => {
+        getTagFor('hiyamashu', 'Metronami', '').then(async (tag) => {
+            const tagCleaned = semver.clean(tag)
+            if (semver.lt(currentVersion, tagCleaned) === false) {
+                // console.log(chalk.white('You are running the latest version of Metronami.'))
+                return resolve(false)
+            }
+
+            // console.log(chalk.greenBright(`New update found: ${tag}`))
+
+            // Url of the package
+            const file = `https://github.com/hiyamashu/Metronami/archive/refs/tags/${tag}.zip`
+            const filePath = appRoot + '/update'
+
+            await download(file, filePath)
+                .then(() => {
+                    console.log(chalk.whiteBright(`Package downloaded: ${tag}`))
+                })
+
+            return resolve(tag)
+        })
+    })
+}
+
+const applyUpdate = (appRoot, tag) => {
+    return new Promise((resolve) => {
+        // Copy save data
+        ncp(appRoot + '/metronami/data', appRoot + '/update/ptn_backup', (err) => {
+            // Delete the old files
+            fs.rmdir(appRoot + '/metronami', { recursive: true }, () => {
+                const fileName = `Metronami-${tag.substring(1)}`
+                // Unzip the file
+                extract(appRoot + `/update/${fileName}.zip`, { dir: appRoot.path })
+
+                // Rename directory
+                setTimeout(() => {
+                    fs.rename(appRoot + `/${fileName}`, appRoot + '/metronami', () => {
+                        // Copy save files back
+                        ncp(appRoot + '/update/ptn_backup', appRoot + '/metronami/data', (err) => {
+                            return resolve()
+                        })
+                    })
+                }, 3000)
+            })
+        })
+    })
+}
+
+module.exports = {
+    getUpdate,
+    applyUpdate
+}
